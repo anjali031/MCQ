90bc2a1999a60fcc1dbbe75a79c54ebf4b0ec093
Anjali Dubey <anjalidubey9844@gmail.com>
2020-01-20 20:31:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpResponse} from '@angular/common/http';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mcq';
+  private apiUrl = 'http://harshraj.pythonanywhere.com/user/api/get-question/?format=json';
+  data: any = {};
+
+  constructor(private http: HttpClient) {
+    console.log('hello fellow user');
+    this.getContacts();
+    this.getData();
+  }
+
+  getData() {
+    return this.http.get(this.apiUrl);
+  }
+
+  getContacts() {
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.data = data;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
